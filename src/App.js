bc2c8e3b23c6c396e2684041fbe3ec0938387164
retin <retin@hexlant.com>
2023-01-31 19:18:25 +0900

    react useState 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+// fun, class
+
+const App = () => {
+
+    // 함수, 상수, 변수, 상태 등등....
+
+    const [name, setName] = useState("teddy")
+
+    const updateName = () => {
+        setName("retin")
+
+    }
+
+
+
+    // 화면 템플릿
+    return (
+        <div>
+            <h1>{name}</h1>
+            <button onClick={()=> updateName()}>변경</button>
+        </div>
+    );
+};
 
 export default App;
